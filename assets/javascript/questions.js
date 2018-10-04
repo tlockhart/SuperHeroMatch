dc3abb3d820f2f13d894cf62969b68518d1d5eb4
ShemikaKW <Shemikakw@gmail.com>
2018-10-04 18:34:00 -0400

    updates

---
+++ assets/javascript/questions.js
@@ -0,0 +1,22 @@
+// Questions and Choices
+
+var questions = [{
+    question: "What's your best feature?",
+    choices: ["Humour", "Intelligence", "Determination", "????"],
+},
+{
+    question: "What's your biggest weakness?",
+    choices: ["Cockiness", "Impulsiviness", "????", "????"],
+},
+{
+    question: "What type of costume would you wear?",
+    choices: ["Spandex", "Something bulletproof", "Must have a Cape", "Whatever"],
+},
+{
+    question: "How would you fight crime?",
+    choices: ["Martial Arts", "????", "????", "????"],
+},  
+{
+    question: "?",
+    choices: ["???", "?????", "????", "????"],
+}],
